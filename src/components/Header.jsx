1d8e3757a7c2a097b2faa15fa8ec7ee738ac5e28
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-09-29 22:53:42 +0530

    UI changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
     <header id="main-header">
       <div id="title">
         <img src={logoImg} alt="A restaurant" />
-        <h1>ReactFood</h1>
+        <h1>ReactFood!</h1>
       </div>
       <nav>
         <Button textOnly onClick={handleShowCart}>
